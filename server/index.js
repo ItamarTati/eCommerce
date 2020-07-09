f481a97489be07290f42112a773c3548b8e6e913
Itamar Tati <itamar.tati1994@gmail.com>
2020-07-09 23:06:26 +0100

    added react app

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('client/public'))
 app.listen(process.env.PORT || 4000, () => console.log('All is running'))
