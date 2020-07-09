9251d8c35b4e95895daf082796c13238feacb27e
Itamar Tati <itamar.tati1994@gmail.com>
2020-07-09 23:12:26 +0100

    making react page appear

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express()
 
-app.use(express.static('client/public'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 app.listen(process.env.PORT || 4000, () => console.log('All is running'))
