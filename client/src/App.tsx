88976d805eb5273249b528eb059ee495b989813b
Itamar Tati <itamar.tati1994@gmail.com>
2020-07-11 09:11:52 +0100

    added auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return <button onClick={() => logout()}>Log Out</button>;
+};
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
+
 
 function App() {
   return (
@@ -9,7 +35,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          
         </p>
+        {Profile()}
+        {LoginButton()}
+        {LogoutButton()}
         <a
           className="App-link"
           href="https://reactjs.org"
